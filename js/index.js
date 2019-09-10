ca6ebe444be916cf61064f2bd1115fea501a9aba
Tatyana Novell <shydalys123@gmail.com>
2019-09-09 19:12:08 -0500

    added a scroll event for the main-navigation so it could change at a specified scrolled point and change the font and background color

--- js/index.js
+++ js/index.js
@@ -5,3 +5,23 @@ busImg.addEventListener('mouseover', (play)=>{
     document.querySelector('.bus').style.animationPlayState = 'running';
     document.querySelector('.bus').style.display = 'block';
 })
+
+const nav = document.querySelector('.main-navigation');
+const navLink = document.querySelectorAll('nav a');
+window.onscroll = () => scrollPosition();
+function scrollPosition(){
+    const position = window.pageYOffset | document.body.scrollTop;
+    if(position > 40){
+    nav.style.color = 'white';
+    nav.style.background= "#ffbf5c";
+    navLink.forEach((arr) => {
+        arr.style.color = 'white';
+    })
+    }else{
+        nav.style.color = 'black';
+        nav.style.background= "white";
+        navLink.forEach((arr) => {
+            arr.style.color = 'black';
+        })
+    }
+};
