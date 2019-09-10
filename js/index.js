05644a8382aaa0ddb84d9c70a440b014fe135d5f
Tatyana Novell <shydalys123@gmail.com>
2019-09-09 23:23:37 -0500

    added function for drag and drop for the main-container images

--- js/index.js
+++ js/index.js
@@ -25,3 +25,21 @@ function scrollPosition(){
         })
     }
 };
+
+//const drag = document.querySelector('dragImage');
+//const drop = document.querySelector('img-box');
+
+function allowDrop(event){
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id)
+}
+
+function drop(event){
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
+
